bb62a287c41eade460e0a93b7042cdca717a093c
PAKIWASI <ullahwasi26@gmail.com>
2026-02-13 02:16:13 +0500

    completed layers

--- include/layer.h
+++ include/layer.h
@@ -13,6 +13,7 @@
 typedef struct Layer {
 
     // Common data for all layers
+
     float* x; // input (1 x m) - Pointer to prev layer output - Needed for backprop
     float* b; // bias (1 x n) - Each neuron of current layer has a bias value
     Matrix W; // weights (m x n) - Maps prev layer neurons to curr layer with a weight (fully connnected)
@@ -20,29 +21,20 @@ typedef struct Layer {
     float* a; // activated output (1 x n) - a = f(z) (element wise activation)  - Non Linearity (Learn complex patterns)
 
     // Gradients for Backpropogation
+
     Matrix dL_dW; // (m x n) - How much current layer's weights effect Loss. (How to change weights to reduce loss)
-    float* dL_db; // (1 x n)
+    // float* dL_db; // (1 x n) - This comes out as equal to dL_dz
     float* dL_dz; // (1 x n)
     float* dL_dx; // (1 x m) - Passed to previous layer - Change in Loss wrt change in input (output of prev layer)
 
     // Dimensions
+
     u16 m;  // Input size - Size of previous layer
     u16 n;  // Ouput size - Size of current layer 
 
-
     // Layer-specific behavior
-    
-    b8 is_output_layer;     // flag determines which act/act_deriv function to use, use da_dz or not
-    void (*activation_fn)(const float* z, float* a, u16 size);
 
-    union {
-        void (*hl_deriv)(const float* z, float* da_dz, u16 size);
-        void (*ol_deriv)(const float* predicted, 
-                const float* true_label, float* dL_dz, u16 size);
-    } act_deriv;
-
-    // Type-specific data (only used by hidden layers)
-    float* da_dz; // derivative cache for ReLU (1 x n)
+    b8 is_output_layer;     // flag determines which act/act_deriv function to use
 
 } Layer;
 
@@ -50,8 +42,9 @@ typedef struct Layer {
 >>> 136 + 16 * 4 + 786 * 16 * 4 + 16 * 4 + 16 * 4 + 786 * 16 * 4 + 16 * 4 + 16 * 4 + 786 * 4 + 16 * 4
 104272
 >>> 104272 / (1024)
-101.828125
+101.828125 KB
 */
+
 
 // Factory functions
 Layer* layer_create_hidden(Arena* arena, u16 m, u16 n);
@@ -69,7 +62,7 @@ void relu_activate(const float* z, float* a, u16 size);
 void softmax_activate(const float* z, float* a, u16 size);
 
 // Backprop functions (implementation details)
-void relu_deriv(const float* z, float* da_dz, u16 size);  // TODO: uses da_dz
+void relu_deriv(const float* z, const float* dL_da, float* dL_dz, u16 size);
 void softmax_crossentropy_deriv(const float* predicted, 
         const float* true_label, float* dL_dz, u16 size);  // y  -> true labels
 
--- src/layer.c
+++ src/layer.c
@@ -1,14 +1,16 @@
 #include "layer.h"
 #include "arena.h"
-#include "common.h"
 #include "matrix.h"
 #include "random.h"
+#include "fast_math.h"
 
 
 Layer* create_common_layer(Arena* arena, u16 m, u16 n);
 void hidden_layer_init_WB(Layer* layer);
 void output_layer_init_WB(Layer* layer);
 
+// cached matrix (transpose of W), we allocate memory for it once
+Matrix mat_T = {0};
 
 
 Layer* layer_create_hidden(Arena* arena, u16 m, u16 n)
@@ -18,10 +20,8 @@ Layer* layer_create_hidden(Arena* arena, u16 m, u16 n)
     Layer* hl = create_common_layer(arena, m, n);
 
     hl->is_output_layer = false;
-    hl->activation_fn = relu_activate;
-    hl->act_deriv.hl_deriv = relu_deriv;
-
-    hl->da_dz = (float*)ARENA_ALLOC_N(arena, float, n);
+    // hl->activation_fn = relu_activate;
+    // hl->act_deriv_fn = relu_deriv;
 
     return hl;
 }
@@ -34,29 +34,114 @@ Layer* layer_create_output(Arena* arena, u16 m, u16 n)
     Layer* ol = create_common_layer(arena, m, n);
 
     ol->is_output_layer = true;
-    ol->activation_fn = softmax_activate;
-    ol->act_deriv.ol_deriv = softmax_crossentropy_deriv;
-
-    ol->da_dz = NULL;   // not needed for output layer 
+    // ol->activation_fn = softmax_activate;
+    // ol->act_deriv_fn = softmax_crossentropy_deriv;
 
     return ol;
 }
 
 
 
-// TODO: this assumes user already inited rng
-//
+// this assumes user already inited rng
 // we have different init methods for hidden, output
 void layer_init_weights_biases(Layer* layer)
 {       
-    CHECK_FATAL(!layer, "layer is null");
-
     if (!layer->is_output_layer) {
         LOG("Using HE Init for Hidden Layer");
         hidden_layer_init_WB(layer);
     } else {
         LOG("Using Xavior Init for Output Layer");
         output_layer_init_WB(layer);
+    }
+}
+
+
+/*
+    Forward Propogation
+    
+    z = x * W + b;
+
+    if input is m and curr layer size is n, then weight matrix is n x m
+    so x * W becomes (1 x m) * (n x m), but we will not do a standard xply
+    x gets xplied to every row of W (n rows), each row has a bias (b (1xn))
+
+    This is more cache friendly
+*/
+void layer_forward(Layer* layer, const float* x)
+{
+    for (u16 i = 0; i < layer->n; i++) {
+        layer->z[i] = 0.0f;
+        for (u16 j = 0; j < layer->m; j++) {
+            layer->z[i] += x[j] * MATRIX_AT(&layer->W, i, j);
+        }
+        layer->z[i] += layer->b[i];
+    }
+}
+
+/*
+    Backward Propogation
+    
+    We get an upstream gradient dL_da
+
+    dL_dz = dL_da * da_dz
+    da_dz = f'(z)
+    dL_dz = dL_da * f'(z)
+
+    need : dL_dW, dL_db, dL_dx
+    we do the derivation of dz_dW, dz_db, dz_dx
+
+    dL_dW = dL_dz * dz_dW = dL_dz * x   (1 x n)T * (1 x m) = (n x m)
+    dL_db = dL_dz * dz_db = dL_dz * 1   (1 x n)
+    dL_dx = dL_dz * dz_dx = dL_dz * W   (1 x n) * (n x m) = (1 x m) - Send downstream
+*/
+void layer_backward(Layer* layer, const float* dL_da)
+{
+        // get dL_dz
+    if (!layer->is_output_layer) {
+        relu_deriv(layer->z, dL_da, layer->dL_dz, layer->n);
+    }
+    else {
+        // for output layer, dL_da passed is actually the true label array
+        softmax_crossentropy_deriv(layer->z, dL_da, layer->dL_dz, layer->n);
+    }
+
+    // dL_dW
+    for (u16 i = 0; i < layer->n; i++) {
+
+        for (u16 j = 0; j < layer->m; j++) {
+            // matrix, arrays accessed row wise - good for cache
+            MATRIX_AT(&layer->dL_dW, i, j) = layer->dL_dz[i] * layer->x[j];
+        }
+    }
+
+    //dL_db = dL_dz
+
+    // dL_dx
+    // transpose the matrix for cache friendly access
+    matrix_T(&mat_T, &layer->W);    // mat is m x n
+
+    for (u16 i = 0; i < layer->m; i++) {
+        for (u16 j = 0; j < layer->n; j++) {
+            // matrix, arrays accessed row wise - good for cache
+            layer->dL_dx[i] += layer->dL_dz[j] * MATRIX_AT(&mat_T, i, j);
+        }
+    }
+}
+
+
+void layer_update_weights(Layer* layer)
+{
+    // Update W using dL_dW
+    u64 total = (u64)layer->m * layer->n;
+
+    for (u64 i = 0; i < total; i++) {
+        layer->W.data[i] += -1 * LEARNING_RATE * layer->dL_dW.data[i];
+    }
+
+    // Update biases using dL_db = dL_dz
+
+    for (u16 i = 0; i < layer->n; i++) {
+        layer->b[i] += -1 * LEARNING_RATE * layer->dL_dz[i];
     }
 }
 
@@ -69,28 +154,57 @@ void relu_activate(const float* z, float* a, u16 size)
     }
 }
 
-void relu_deriv(const float* z, float* da_dz, u16 size)
+void relu_deriv(const float* z, const float* dL_da, float* dL_dz, u16 size)
 {
-    // hidden layer so we have da_dz stored in layer
-    
+    // for (u16 i = 0; i < size; i++) {
+    //     da_dz[i] = z[i] >= 0 ? 1.0f : 0.0f;
+    // }
+
     for (u16 i = 0; i < size; i++) {
-        da_dz[i] = z[i] >= 0 ? 1.0f : 0.0f;
+        // dL_dz[i] = dL_da[i] * ((z[i] >= 0) ? 1.0f : 0.0f);
+        dL_dz[i] = (z[i] >= 0) ? dL_da[i] : 0;  // jacobian
     }
 }
 
 void softmax_activate(const float* z, float* a, u16 size)
 {
-
+    // Numerical stability: subtract max to prevent overflow
+    // Prevents overflow when z values are large
+    float max_z = z[0];
+    for (u16 i = 1; i < size; i++) {
+        if (z[i] > max_z) { max_z = z[i]; }
+    }
+    
+    // Compute exp(z - max) and sum
+    float sum = 0;
+    for (u16 i = 0; i < size; i++) {
+        a[i] = fast_exp(z[i] - max_z);  // Store exp values
+        sum += a[i];
+    }
+    
+    // Normalize
+    for (u16 i = 0; i < size; i++) {
+        a[i] /= sum;
+    }
 }
 
-// p = predicted vector , y = true label vector (hot coded)
-// dL_dz = (Starting Loss deriv) (got by shortcut)
-// The shortcut simplies getting dL_dz directly by:
-// dL_dz = predicted - true
+/*
+Cross-Entropy Loss:
+L = -Σ y_i × log(p_i)
+where:
+- y_i = true label (one-hot encoded)
+- p_i = predicted probability (softmax output)
+
+When computing dL/dz , the derivative simplifies remarkably:
+
+dL/dz_i = p_i - y_i
+*/
 void softmax_crossentropy_deriv(const float* predicted, 
                 const float* true_label, float* dL_dz, u16 size)
 {
-
+    for (u16 i = 0; i < size; i++) {
+        dL_dz[i] = predicted[i] - true_label[i];
+    }
 }
 
 
@@ -104,32 +218,38 @@ Layer* create_common_layer(Arena* arena, u16 m, u16 n)
 
     u64 mark = arena_get_mark(arena);
 
-    Layer* l = (Layer*)arena_alloc(arena, sizeof(Layer));
+    Layer* l = ARENA_ALLOC(arena, Layer);
      
     l->x = NULL;   // will be set layer
 
     // init to all zeros
-    l->b = (float*)ARENA_ALLOC_ZERO_N(arena, float, n);
+    l->b = ARENA_ALLOC_ZERO_N(arena, float, n);
 
-    l->W.m = m; 
-    l->W.n = n; 
-    l->W.data = (float*)ARENA_ALLOC_N(arena, float, (u64)n * m);
+    l->W.m = n; 
+    l->W.n = m; 
+    l->W.data = ARENA_ALLOC_N(arena, float, (u64)n * m);
 
-    l->z = (float*)ARENA_ALLOC_N(arena, float, n);
-    l->a = (float*)ARENA_ALLOC_N(arena, float, n);
+    l->z = ARENA_ALLOC_N(arena, float, n);
+    l->a = ARENA_ALLOC_N(arena, float, n);
 
-    l->dL_dW.m = m;
-    l->dL_dW.n = n;
-    l->dL_dW.data = (float*)ARENA_ALLOC_N(arena, float, (u64)n * m);
+    l->dL_dW.m = n;
+    l->dL_dW.n = m;
+    l->dL_dW.data = ARENA_ALLOC_N(arena, float, (u64)n * m);
 
-    l->dL_db = (float*)ARENA_ALLOC_N(arena, float, n);
-    l->dL_dz = (float*)ARENA_ALLOC_N(arena, float, n);
-    l->dL_dx = (float*)ARENA_ALLOC_N(arena, float, m);
+    // l->dL_db = (float*)ARENA_ALLOC_N(arena, float, n);
+    l->dL_dz = ARENA_ALLOC_N(arena, float, n);
+    l->dL_dx = ARENA_ALLOC_N(arena, float, m);
 
     l->m = m;
     l->n = n;
 
     LOG("allocated %lu for common layer", arena_used(arena) - mark);
+
+    if (mat_T.data == NULL) {
+        mat_T.m = m;
+        mat_T.n = n;
+        mat_T.data = ARENA_ALLOC_N(arena, float, (u64)n * m);
+    }
 
     return l;
 }
